1fb700ad1d48b6c99c6130b12c1fc50c42133926
Corey <coreyshanahan01@gmail.com>
2023-11-07 21:16:26 +0000

    billboard sorta working?
    Signed-off-by: Corey <coreyshanahan01@gmail.com>

--- Assets/Scripts/BurgerHealth.cs
+++ Assets/Scripts/BurgerHealth.cs
@@ -8,8 +8,10 @@ public class BurgerHealth : MonoBehaviour
     [SerializeField] private Image _healthBar;
     [SerializeField] private float _speed = 5f;
     [SerializeField] private Transform _canvasTransform;
+    [SerializeField] private RectTransform _canvasRectRansform;
     [SerializeField] private Vector3 _canvasOffset;
     private bool isCooking = false;
+    private Transform _cameraTransform;
 
     public bool IsCooking
     {
@@ -20,13 +22,21 @@ public class BurgerHealth : MonoBehaviour
     void Start()
     {
         _healthBar.fillAmount = 0;
+        _cameraTransform = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var pos = new Vector3(transform.position.x - Camera.main.transform.position.x, 0, 0);
-        _canvasTransform.rotation = Quaternion.LookRotation(pos);
+        var pos = new Vector3(_canvasTransform.position.x - _cameraTransform.position.x, 0, 0);
+        // _canvasTransform.rotation = Quaternion.LookRotation(pos);
+        // var rot = Quaternion.LookRotation(pos);
+        _canvasRectRansform.rotation = Quaternion.LookRotation(pos);
+        
+        // Debug.Log(pos);
+        // Debug.Log(_cameraTransform.rotation);
+        // Debug.Log(_canvasTransform.rotation);
+
 
         _canvasTransform.position = transform.position + _canvasOffset;
 
