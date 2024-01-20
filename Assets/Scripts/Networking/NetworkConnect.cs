babc55573d1c1c34ce1d139624d2993f6274f8b3
Corey <coreyshanahan01@gmail.com>
2024-01-20 21:09:57 +0000

    player movement synced with other players working
    Signed-off-by: Corey <coreyshanahan01@gmail.com>

--- Assets/Scripts/Networking/NetworkConnect.cs
+++ Assets/Scripts/Networking/NetworkConnect.cs
@@ -5,10 +5,6 @@ using Unity.Netcode;
 
 public class NetworkConnect : MonoBehaviour
 {
-    public void Create(){
-        NetworkManager.Singleton.StartHost();
-    }
-
     public void Join() {
         NetworkManager.Singleton.StartClient();
     }
